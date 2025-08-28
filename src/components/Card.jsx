aec2fed7ebf430ce66a84c6ac481bc1157ad7a8f
SebastianLozanoSiza <jsebastianlozano@uts.edu.co>
2025-08-28 09:03:04 -0500

    Se usa la renderizacion de listas y de condicionales

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,49 +1,39 @@
+import { products } from "../data.js"
+
 export default function Card() {
+    console.log(products);
+
     return (
         <>
             <h2 className="my-16 text-center text-[40px]">Carrito de compras</h2>
             <main className="grid grid-cols-3 gap-5 w-[1200px] mx-auto">
-                <figure className="bg-gray-200 flex flex-col items-center mb-8 rounded-[12px]">
-                    <img className="p-9 w-[60%]" src={"/genovesa.png"} alt="" />
-                    <h3 className="text-[35px] text-center">Genovesa</h3>
-                    <section className="flex flex-col items-center gap-4">
-                        <p className="text-[20px]">Deliciosas Genovesas</p>
-                        <a className="bg-red-700 py-2 text-[20px] px-6 rounded-xl text-white mb-10" href="">Add Carr</a>
-                    </section>
-                </figure>
-                <figure className="bg-gray-200 flex flex-col items-center mb-8 rounded-[12px]">
-                    <img className="p-9 w-[60%]" src={"/cobitos.png"} alt="" />
-                    <h3 className="text-[35px] text-center">Cobitos</h3>
-                    <section className="flex flex-col items-center gap-4">
-                        <p className="text-[20px] text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <a className="bg-red-700 py-2 text-[20px] px-6 rounded-xl text-white" href="">Add Carr</a>
-                    </section>
-                </figure>
-                <figure className="bg-gray-200 flex flex-col items-center mb-8 rounded-[12px]">
-                    <img className="p-9 w-[60%]" src={"/reposteria.png"} alt="" />
-                    <h3 className="text-[35px] text-center">Reposteria</h3>
-                    <section className="flex flex-col items-center gap-4">
-                        <p className="text-[20px] text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <a className="bg-red-700 py-2 text-[20px] px-6 rounded-xl text-white" href="">Add Carr</a>
-                    </section>
-                </figure>
-                <figure className="bg-gray-200 flex flex-col items-center mb-8 rounded-[12px]">
-                    <img className="p-9 w-[60%]" src={"/snacks.png"} alt="" />
-                    <h3 className="text-[35px] text-center">Snacks</h3>
-                    <section className="flex flex-col items-center gap-4">
-                        <p className="text-[20px] text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <a className="bg-red-700 py-2 text-[20px] px-6 rounded-xl text-white" href="">Add Carr</a>
-                    </section>
-                </figure>
-                <figure className="bg-gray-200 flex flex-col items-center mb-8 rounded-[12px]">
-                    <img className="p-9 w-[60%]" src={"/tortas.png"} alt="" />
-                    <h3 className="text-[35px] text-center">Tortas</h3>
-                    <section className="flex flex-col items-center gap-4">
-                        <p className="text-[20px] text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <a className="bg-red-700 py-2 text-[20px] px-6 rounded-xl text-white mb-10" href="">Add Carr</a>
-                    </section>
-                </figure>
+                {products.map((product, index) => (
+                    <Product
+                        key={product.id}
+                        productProp={product}
+                        index={index}
+                    />
+                ))}
             </main>
+        </>
+    )
+}
+
+function Product(props) {
+    return (
+        /* Conditional Rendering = renderizacion condicional
+        usando operador ternario 
+        */
+        <>
+            <figure className={`bg-gray-200 flex flex-col items-center mb-8 rounded-[12px]
+                ${props.index % 2 === 0 ? "col-span-2" : "col-span-1"}`}>
+                <img className="p-9 w-[60%]" src={props.productProp.imagen} alt="" />
+                <h3 className="text-[35px] text-center">{props.productProp.nombre}</h3>
+                <section className="flex flex-col items-center gap-4">
+                    <p className="text-[20px]">{props.productProp.Descripcion}</p>
+                    <a className="bg-red-700 py-2 text-[20px] px-6 rounded-xl text-white mb-10" href="">Add Carr</a>
+                </section>
+            </figure>
         </>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+        id: 1,
+        imagen: "/genovesa.png",
+        nombre: "Genovesa",
+        Descripcion: "Deliciosas Genovesas"
+    },
+    {
+        id: 2,
+        imagen: "/zepeline.png",
+        nombre: "Zepeline",
+        Descripcion: "Deliciosos Zepelines"
+    },
+    {
+        id: 3,
+        imagen: "/reposteria.png",
+        nombre: "Reposteria",
+        Descripcion: "Deliciosos Postres"
+    },
+    {
+        id: 4,
+        imagen: "/tortas.png",
+        nombre: "Tortas",
+        Descripcion: "Deliciosas Tortas"
+    }
+]
